dea22561e0e739013f689c9377a23978edd7ee79
unknown <ungquangtri@gmail.com>
2024-11-12 22:24:33 +0700

    Add cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import { database } from "./database/index.js";
 import { userRouter, authenRouter } from "./routes/index.js";
+import cors from "cors";
 const app = express();
 const PORT = 3002;
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/user", userRouter);
 app.use("/authen", authenRouter);
 app.listen(PORT, async () => {
