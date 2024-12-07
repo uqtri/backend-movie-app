69e217972af37747560256255245e65fc23ab4b6
unknown <ungquangtri@gmail.com>
2024-12-07 11:03:02 +0700

    Verify where user is banned or not in authen

--- controllers/authen.js
+++ controllers/authen.js
@@ -1,17 +1,26 @@
+import { httpStatusCode } from "../httpStatusCode/httpStatusCode.js";
 import { authenRepository } from "../repositories/index.js";
 
 const authen = async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await authenRepository.authen({ username, password });
-    if (user) {
+    if (user.isBanned === false) {
       res.status(200).json({
         message: "Logined",
+        httpStatusCode: httpStatusCode.OKE,
+      });
+    } else {
+      res.status(httpStatusCode.BAD_REQUEST).json({
+        status: "Banned",
+        message: user.message,
+        statusCode: httpStatusCode.BAD_REQUEST,
       });
     }
   } catch (error) {
     res.status(400).json({
       message: error.toString().slice(6),
+      statusCode: httpStatusCode.BAD_REQUEST,
     });
   }
 };
